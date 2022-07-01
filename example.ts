0029fce85e2446fb90c2bd7f5d15ad3ccf554843
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-07-01 18:48:26 +0100

    feat: add import map

--- example.ts
+++ example.ts
@@ -1,5 +1,5 @@
-import TypedEventEmitter from './mod.ts';
-import EventEmitter from 'https://deno.land/std@0.146.0/node/events.ts';
+import TypedEventEmitter from 'events.ts';
+import EventEmitter from 'events';
 
 const emitter = new EventEmitter() as TypedEventEmitter<{ message(message: string): void }>;
 
